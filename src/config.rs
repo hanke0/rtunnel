ca28d3925aa2c267f7c506c819e0013c285e92e0
hanke <hanke0@outlook.com>
2025-05-28 10:02:17 +0800

    first design of config

---
+++ src/config.rs
@@ -0,0 +1,42 @@
+use serde::Deserialize;
+use serde::de::DeserializeOwned;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Deserialize)]
+pub struct ServerConfig {
+    pub endpoint: String,
+    pub private_key: String,
+    pub ca: String,
+    pub services: Vec<ServerService>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ClientConfig {
+    pub name: String,
+    pub server_endpoint: String,
+    pub public_key: String,
+    pub encryption: Option<String>,
+    pub services: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ServerService {
+    pub name: String,
+    pub allowed_clients: Vec<String>,
+    pub bind: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ClientService {
+    pub name: String,
+    pub connect: String,
+}
+
+pub fn from_file<T: DeserializeOwned>(path: &str) -> Result<T, Box<dyn std::error::Error>> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let config: T = toml::from_str(&contents)?;
+    Ok(config)
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::process::exit;
+
+use rtunnel::config::{ServerConfig, from_file};
+use tokio::runtime::Builder;
+
+fn main() {
+    let cfg = from_file::<ServerConfig>("server.toml");
+    if cfg.is_err() {
+        eprintln!("Failed to load config: {}", cfg.err().unwrap());
+        exit(1);
+    }
+    let _cfg = cfg.unwrap();
+    let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+    rt.block_on(async {
+        println!("hello");
+    });
+}
