361f980ad55ad6724605dd11ad483ec33a782589
hanke <hanke0@outlook.com>
2025-05-28 10:46:32 +0000

    udpate

---
+++ src/encryption.rs
@@ -0,0 +1,8 @@
+use tokio::net::TcpStream;
+
+
+pub async fn client_handshake(stream: &mut TcpStream) -> Result<(), std::io::Error> {
+}
+
+pub async fn server_handshake(stream: &mut TcpStream) -> Result<(), std::io::Error> {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod config;
+mod transport;
+mod encryption;
+use config;
 use std::process::exit;
-use config::{ServerConfig, from_file};
 use tokio::runtime::Builder;
 
 fn main() {
-    let cfg = from_file::<ServerConfig>("server.toml");
+    let cfg = config::from_file::<config::ServerConfig>("server.toml");
     if cfg.is_err() {
         eprintln!("Failed to load config: {}", cfg.err().unwrap());
         exit(1);
