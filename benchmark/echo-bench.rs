8d7c2df9fff1a370f6f17a3d74db90c12fb1125f
hanke <hanke0@outlook.com>
2025-11-20 20:00:14 +0800

    improve echo benchmark metrics

--- benchmark/echo-bench.rs
+++ benchmark/echo-bench.rs
@@ -10,6 +10,19 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::task::JoinSet;
 
+#[derive(Default)]
+struct Metrics {
+    connect_success: AtomicI64,
+    connect_failed: AtomicI64,
+    connect_spend_ms: AtomicI64,
+    transfer_bytes: AtomicI64,
+    transfer_spend_ms: AtomicI64,
+    transfer_success: AtomicI64,
+    transfer_failed: AtomicI64,
+}
+
+type MetricsRef = Arc<Metrics>;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let addr = env::args().nth(1).unwrap();
@@ -20,9 +33,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let loops = env::args().nth(6).unwrap().parse::<usize>().unwrap();
     run_server(&listen_addr).await?;
 
-    let success = Arc::new(AtomicI64::new(0));
-    let failed = Arc::new(AtomicI64::new(0));
-    let spend = Arc::new(AtomicI64::new(0));
+    let metrics = Arc::new(Metrics::default());
     let mut set = JoinSet::new();
     for _ in 0..concurrent {
         set.spawn(client_task(
@@ -30,9 +41,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             times,
             bytes,
             loops,
-            success.clone(),
-            failed.clone(),
-            spend.clone(),
+            metrics.clone(),
         ));
     }
     set.join_all().await;
@@ -41,41 +50,104 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("times: {times}");
     println!("bytes: {bytes}");
     println!("loops: {loops}");
-    println!("Success: {}", success.load(Ordering::Acquire));
-    println!("Failed: {}", failed.load(Ordering::Acquire));
-    println!("Spend: {}ms", spend.load(Ordering::Acquire));
-    let throughout = times as f64 * bytes as f64 * success.load(Ordering::Acquire) as f64
-        / (spend.load(Ordering::Acquire) as f64 / 1000.)
+    println!(
+        "connect_success: {}",
+        metrics.connect_success.load(Ordering::SeqCst)
+    );
+    println!(
+        "connect_failed: {}",
+        metrics.connect_failed.load(Ordering::SeqCst)
+    );
+    println!(
+        "connect_spend_ms: {}",
+        metrics.connect_spend_ms.load(Ordering::SeqCst)
+            / metrics.connect_success.load(Ordering::SeqCst)
+    );
+    println!(
+        "transfer_success: {}",
+        metrics.transfer_success.load(Ordering::SeqCst)
+    );
+    println!(
+        "transfer_failed: {}",
+        metrics.transfer_failed.load(Ordering::SeqCst)
+    );
+    println!(
+        "transfer_spend_ms: {}",
+        metrics.transfer_spend_ms.load(Ordering::SeqCst)
+    );
+    println!(
+        "transfer_bytes: {}",
+        metrics.transfer_bytes.load(Ordering::SeqCst)
+    );
+    let throughout = metrics.transfer_bytes.load(Ordering::SeqCst) as f64
+        / (metrics.transfer_spend_ms.load(Ordering::SeqCst) as f64)
+        * 1000.
         / 1024.;
+
     println!("Throughput: {:.3}KB/s", throughout);
     Ok(())
 }
 
-async fn client_task(
-    addr: String,
-    times: usize,
-    bytes: usize,
-    loops: usize,
-    success: Arc<AtomicI64>,
-    failed: Arc<AtomicI64>,
-    spend: Arc<AtomicI64>,
-) {
+async fn client_task(addr: String, times: usize, bytes: usize, loops: usize, metrics: MetricsRef) {
     for _ in 0..times {
         let start = Instant::now();
-        let result = run_client(&addr, loops, bytes).await;
-        let end = Instant::now();
-        match result {
-            Err(e) => {
-                eprintln!("client task failed: {:#}", e);
-                failed.fetch_add(1, Ordering::Release);
+        let mut stream = match TcpStream::connect(&addr).await {
+            Ok(s) => s,
+            Err(_) => {
+                metrics.connect_failed.fetch_add(1, Ordering::SeqCst);
+                return;
             }
-            Ok(_) => {
-                success.fetch_add(1, Ordering::Release);
-                spend.fetch_add(
-                    end.duration_since(start).as_millis() as i64,
-                    Ordering::Release,
-                );
+        };
+        match stream.write_i8(1).await {
+            Ok(_) => {}
+            Err(_) => {
+                metrics.connect_failed.fetch_add(1, Ordering::SeqCst);
+                return;
             }
+        };
+        match stream.read_i8().await {
+            Ok(_) => {}
+            Err(_) => {
+                metrics.connect_failed.fetch_add(1, Ordering::SeqCst);
+                return;
+            }
+        };
+        metrics.connect_success.fetch_add(1, Ordering::SeqCst);
+        let spend = Instant::now().duration_since(start).as_millis();
+        metrics
+            .connect_spend_ms
+            .fetch_add(spend as i64, Ordering::SeqCst);
+
+        if bytes == 0 {
+            return;
+        }
+        let (mut reader, mut writer) = stream.into_split();
+        let mut buf = vec![0; bytes];
+        for _ in 0..loops {
+            let start = Instant::now();
+            match writer.write_all(&buf).await {
+                Ok(_) => {}
+                Err(_) => {
+                    metrics.transfer_failed.fetch_add(1, Ordering::SeqCst);
+                    return;
+                }
+            };
+            match reader.read_exact(&mut buf).await {
+                Ok(_) => {
+                    metrics.transfer_success.fetch_add(1, Ordering::SeqCst);
+                    let spend = Instant::now().duration_since(start).as_millis();
+                    metrics
+                        .transfer_spend_ms
+                        .fetch_add(spend as i64, Ordering::SeqCst);
+                    metrics
+                        .transfer_bytes
+                        .fetch_add(bytes as i64, Ordering::SeqCst);
+                }
+                Err(_) => {
+                    metrics.transfer_failed.fetch_add(1, Ordering::SeqCst);
+                    return;
+                }
+            };
         }
     }
 }
@@ -105,18 +177,4 @@ async fn handle_server_stream(mut socket: TcpStream) -> Result<(), Box<dyn Error
         }
         socket.write_all(&buf[0..n]).await?;
     }
-}
-
-async fn run_client(addr: &str, loops: usize, bytes: usize) -> Result<(), Box<dyn Error>> {
-    let stream = TcpStream::connect(addr).await?;
-    if bytes == 0 {
-        return Ok(());
-    }
-    let (mut reader, mut writer) = stream.into_split();
-    let mut buf = vec![0; bytes];
-    for _ in 0..loops {
-        writer.write_all(&buf).await?;
-        reader.read_exact(&mut buf).await?;
-    }
-    Ok(())
 }
